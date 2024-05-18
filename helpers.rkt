ecb5af13f9ab0d4f99379902c7a8cf76e391e197
Efi <gje87@hotmail.com>
2024-05-18 23:26:23 +0200

    cleanup

--- helpers.rkt
+++ helpers.rkt
@@ -26,50 +26,6 @@
 
 (define (rxtag s) (regexp (format "(?i:^~a: (.+)$)" s)))
 
-;(provide custom-parse)
-;(define (custom-parse file)
-;  (let ([ppl (make-immutable-hash
-;               (foldl (λ (line acc)
-;                         (if (empty? line)
-;                           acc
-;                           (let ([parts (string-split line)])
-;                             (append acc (list (list (first parts) (string-join (rest (drop-right parts 1))) (last parts)))))))
-;                      '()
-;                      (file->lines "people.txt")))])
-;    (for/fold ([body-lv null]
-;               [story-lv null]
-;               [lr-lv null]
-;               [block-class ""]
-;               #:result (add-newlines (append body-lv (divc "story" (append story-lv (divc "right" lr-lv))))))
-;      ([data (in-list (file->lines file))])
-;      (cond
-;        [(regexp-match? #rx"^--$" data)
-;         (if (string=? "" block-class)
-;           (values body-lv story-lv lr-lv "left")
-;           (if (string=? block-class "right")
-;             (values (append body-lv (divc "story" (append story-lv (divc "right" lr-lv)))) null null "left")
-;             (values body-lv (append story-lv (divc "left" lr-lv)) null "right")))]
-;        [(regexp-match? (rxtag "img") data)
-;         (let ([url (~a "https://efimero.github.io/xenia-images/" (second (regexp-match (rxtag "img") data)))])
-;           (values body-lv story-lv (append lr-lv (e->sl (a 'target: "_blank" 'href: url (img 'src: url)))) block-class))]
-;        [(regexp-match? (rxtag "sub") data)
-;         (values body-lv story-lv (append lr-lv (list (literal (~a "Submitted by: " (rec-parse (second (regexp-match (rxtag "sub") data))) "<br/>")))) block-class)]
-;        [(regexp-match? (rxtag "sref") data)
-;         (values body-lv story-lv (append lr-lv (e->sl (span (literal "Submitted by: ") (let ([subm (hash-ref ppl (second (regexp-match (rxtag "sref") data)))]) (a 'href: (second subm) (first subm))) (br)))) block-class)]
-;        [(regexp-match? (rxtag "aref") data)
-;         (values body-lv story-lv (append lr-lv (e->sl (div 'class: "author" (literal "Author: ") (let ([author (hash-ref ppl (second (regexp-match (rxtag "aref") data)))]) (a 'href: (second author) (first author))) (br)))) block-class)]
-;        [(regexp-match? (rxtag "author") data)
-;         (values body-lv story-lv (append lr-lv (e->sl (div 'class: "author" (literal (~a "Author: " (rec-parse (second (regexp-match (rxtag "author") data))) "<br/>"))))) block-class)]
-;        [(regexp-match? (rxtag "source") data)
-;         (values body-lv story-lv (append lr-lv (list (literal (~a "Source: " (rec-parse (second (regexp-match (rxtag "source") data))) "<br/>")))) block-class)]
-;        [(regexp-match? (rxtag "license") data)
-;         (values body-lv story-lv (append lr-lv (e->sl (div 'class: "license" (literal (~a "License: " (rec-parse (second (regexp-match (rxtag "license") data))) "<br/>"))))) block-class)]
-;        [(regexp-match? (rxtag "time") data)
-;         (let ([t (second (regexp-match (rxtag "time") data))])
-;           (values body-lv story-lv (append lr-lv (list (literal (~a (xml->string (element 'time 'datetime: t 'title: t (~t (iso8601->date t) "d MMMM y"))) "<br/>")))) block-class))]
-;        [else
-;          (values body-lv story-lv (append lr-lv (list (literal (rec-parse data)))) block-class)]))))
-
 (define img-repo "https://efimero.github.io/xenia-images/")
 
 (provide custom-parse)
@@ -95,9 +51,7 @@
         '()
         (in-indexed sections))
       (λ (a b)
-        (string<? (first a) (first b)))))
-    )
-  )
+        (string<? (first a) (first b)))))))
 
 (define (parse-dispatch ppl str)
   (add-newlines (map
